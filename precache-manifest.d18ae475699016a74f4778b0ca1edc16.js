934b1d6c4301555b59dbe3dc62572a1d1bdc48ac
lwinkyawmyat <mglwinkyawmyat@gmail.com>
2018-10-23 18:55:36 +0630

    Updates

---
+++ precache-manifest.d18ae475699016a74f4778b0ca1edc16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8842e7b8b9be0c7ef790",
+    "url": "/react-mm-text/static/js/runtime~main.8842e7b8.js"
+  },
+  {
+    "revision": "fca90e724f2f042d541e",
+    "url": "/react-mm-text/static/js/main.fca90e72.chunk.js"
+  },
+  {
+    "revision": "833c678fb26b97803636",
+    "url": "/react-mm-text/static/js/1.833c678f.chunk.js"
+  },
+  {
+    "revision": "fca90e724f2f042d541e",
+    "url": "/react-mm-text/static/css/main.f150ba0d.chunk.css"
+  },
+  {
+    "revision": "833c678fb26b97803636",
+    "url": "/react-mm-text/static/css/1.15273ed5.chunk.css"
+  },
+  {
+    "revision": "60f5738780d0841ec215259a4775fa49",
+    "url": "/react-mm-text/index.html"
+  }
+];
